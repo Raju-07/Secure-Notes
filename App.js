9935f429badb8c0429a8308a1b84d4d4a3ad757c
raju-07 <rajuyadav782760@gmail.com>
2026-03-21 14:12:03 +0530

    change the version to 54.0.0

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Just Fixed the Expo version to the 54.0.0</Text>
       <StatusBar style="auto" />
     </View>
   );
